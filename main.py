0e1488517b9111535c090ba7a37ad4dcb07071be
Monalisa Sahoo <65477771+monalisamsteccentric@users.noreply.github.com>
2025-12-31 16:49:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 
+import requests
+
+SERP_API_KEY = "YOUR_KEY"
+
+def google_search(query):
+    url = "https://serpapi.com/search"
+    params = {
+        "q": query,
+        "api_key": SERP_API_KEY,
+        "engine": "google"
+    }
+    return requests.get(url, params=params).json()
